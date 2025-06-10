1f563b5dc788e8e964ec58b0343d6f7a56de8195
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-06-10 13:35:51 +0200

    Extend validation of UIGraphs

--- puma/apps/android/FSMTEST_util/puma_fsm.py
+++ puma/apps/android/FSMTEST_util/puma_fsm.py
@@ -52,7 +52,7 @@ class State(ABC):
         self.transitions.append(Transition(self, to_state, ui_actions))
 
     @abstractmethod
-    def validate(self, driver: PumaDriver) -> bool:  # TODO: type of driver, or do we want wrapped method?
+    def validate(self, driver: PumaDriver) -> bool:
         pass
 
     def __repr__(self):
@@ -79,6 +79,22 @@ class Transition:
     to_state: State
     ui_actions: Callable[[Any], None]  # TODO: typing
 
+def _shortest_path(start: State, destination: State | str):
+    visited = set()
+    queue = deque([(start, [])])
+    while queue:
+        state, path = queue.popleft()
+        # if this is a path to the desired state, return the path
+        if state == destination or state.name == destination:
+            return path
+        # we do not want cycles: skip paths to already visited states
+        if state in visited:
+            continue
+        visited.add(state)
+        # take a step in all possible directions
+        for transition in state.transitions:
+            queue.append((transition.to_state, path + [transition]))
+    return None
 
 class PumaUIGraphMeta(type):
     def __new__(cls, name, bases, namespace):
@@ -99,22 +115,42 @@ class PumaUIGraphMeta(type):
                 transitions.append(value)
         new_class.states = states
         new_class.transitions = [transition for state in states for transition in state.transitions]
+        new_class.initial_state = next(s for s in states if s.initial_state)
 
         ## validation
-        # TODO: make separate validate method
+        PumaUIGraphMeta._validate_graph(states)
+
+        return new_class
+
+    @staticmethod
+    def _validate_graph(states: List[State]):  # TODO: make unit test for this validation
         # only 1 initial state
         initial_states = [s for s in states if s.initial_state]
         if len(initial_states) == 0:
             raise ValueError(f'Graph needs an initial state')
         elif len(initial_states) > 1:
             raise ValueError(f'Graph can only have 1 initial state, currently more defined: {initial_states}')
-        new_class.initial_state = initial_states[0]
-        # every state except initial state needs transitions
-        # TODO: further validation: you need to be able to go from  the initial state to each other state and back
-        # No duplicate transitions: only one transition between each 2 states
-        # initial state cannot be FState, FStates need parent state
+        initial_state = initial_states[0]
 
-        return new_class
+        # initial state cannot be FState
+        if isinstance(initial_state, FState):
+            raise ValueError(f'Initial state ({initial_state}) Cannot be an FState')
+
+        # FStates need parent state
+        fstate_without_parent = [s for s in states if isinstance(s, FState) and s.parent_state is None]
+        if fstate_without_parent:
+            raise ValueError(f'FStates without parent are not allowed: {fstate_without_parent}')
+
+        #you need to be able to go from  the initial state to each other state and back
+        unreachable_states = [s for s in states if not s.initial_state and not(bool(_shortest_path(initial_state, s)) and bool(_shortest_path(s, initial_state)))]
+        if unreachable_states:
+            raise ValueError(f'Some states cannot be reached from the initial state, or cannot go back to the initial state: {unreachable_states}')
+        # No duplicate transitions: only one transition between each 2 states
+        for s in states:
+            seen = set()
+            duplicates = {t.to_state for t in s.transitions if t.to_state in seen or seen.add(t.to_state)}
+            if duplicates:
+                raise ValueError(f"State {s} has invalid transitions: multiple transitions defined to neighboring state(s) {duplicates}")
 
 
 def _safe_func_call(func, **kwargs):
@@ -221,23 +257,7 @@ class PumaUIGraph(metaclass=PumaUIGraphMeta):
         """
         Gets the shortest path (in number of transitions) to the desired state.
         """
-        start = self.current_state
-        visited = set()
-        queue = deque([(start, [])])
-
-        while queue:
-            state, path = queue.popleft()
-            # if this is a path to the desired state, return the path
-            if state == destination or state.name == destination:
-                return path
-            # we do not want cycles: skip paths to already visited states
-            if state in visited:
-                continue
-            visited.add(state)
-            # take a step in all possible directions
-            for transition in state.transitions:
-                queue.append((transition.to_state, path + [transition]))
-        return None
+        return _shortest_path(self.current_state, destination)
 
 
 def action(to_state: State):
@@ -265,3 +285,17 @@ def action(to_state: State):
         return wrapper
 
     return decorator
+
+class TestFsm(PumaUIGraph):
+    start_state = SimpleState("start state", [], True)
+    state1 = SimpleState("state 1", [], parent_state=start_state)
+    state1_1 = SimpleState("state 1.1", [], parent_state=state1)
+    state2 = SimpleState("state 2", [], parent_state=start_state)
+
+    start_state.to(state1, None)
+    state1.to(state1_1, None)
+    start_state.to(state2, None)
+
+
+if __name__ == '__main__':
+    print("testing")
