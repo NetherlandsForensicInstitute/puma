ee403530fc0c8c27af0c661a9491b8ddfe4d3bf7
Angelina-C <36659310+Angelina-C@users.noreply.github.com>
2025-06-11 15:30:42 +0200

    FSM2-feature: Add pydoc to TeleGuard

--- puma/apps/android/FSMTEST_teleguard/teleguard.py
+++ puma/apps/android/FSMTEST_teleguard/teleguard.py
@@ -1,10 +1,11 @@
 from appium.webdriver.common.appiumby import AppiumBy
-
 from puma.apps.android.FSMTEST_util.puma_driver import PumaDriver
-from puma.apps.android.FSMTEST_util.puma_fsm import SimpleState, StateGraph, action, simple_popup_handler, ContextualState
+from puma.apps.android.FSMTEST_util.puma_fsm import SimpleState, StateGraph, action, simple_popup_handler, ContextualState, compose_clicks
+from puma.apps.android.appium_actions import supported_version
 
 APPLICATION_PACKAGE = 'ch.swisscows.messenger.teleguardapp'
 
+# TODO: Move to separate xpaths file?
 CONVERSATION_STATE_TELEGUARD_HEADER = '//android.view.View[@content-desc="TeleGuard"]'
 CONVERSATION_STATE_HAMBURGER_MENU = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View[3]'
 CONVERSATION_STATE_SETTINGS_BUTTON = '//android.widget.ImageView[@content-desc="Settings"]'
@@ -13,57 +14,87 @@ CONVERSATION_STATE_TELEGUARD_STATUS = '//android.view.View[@content-desc="Online
 
 CHAT_STATE_CONVERSATION_NAME = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.widget.ImageView[2][@content-desc]'
 CHAT_STATE_TEXT_FIELD = '//android.widget.EditText'
-CHAT_STATE_MICROPHONE_BUTTON = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[4]' # TODO: This is gone, when draft message is present. Use something else.
+CHAT_STATE_MICROPHONE_BUTTON = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[4]'
 CHAT_STATE_SEND_BUTTON = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[3]'
 
-
-def go_to_settings(driver: PumaDriver):
-    print(f"click on settings button with driver {driver}")
-    driver.click(CONVERSATION_STATE_HAMBURGER_MENU)
-    driver.click(CONVERSATION_STATE_SETTINGS_BUTTON)
-
-
 def go_to_chat(driver: PumaDriver, conversation: str):
-    xpath = f'//android.widget.ImageView[contains(lower-case(@content-desc), "{conversation.lower()}")] | \
-                    //android.view.View[contains(lower-case(@content-desc), "{conversation.lower()}")]'
-    driver.driver.find_elements(by=AppiumBy.XPATH, value=xpath)[
-        -1].click()  # TODO Fix this, there should a ticket somewhere (#104)
+    """
+    Navigates to a specific chat conversation in the application.
+
+    This function constructs an XPath to locate and click on a conversation element
+    based on the conversation name. It is designed to be used within a state transition
+    to navigate to a specific chat state.
+
+    :param driver: The PumaDriver instance used to interact with the application.
+    :param conversation: The name of the conversation to navigate to.
+    """
+    xpath = f'//android.widget.ImageView[contains(lower-case(@content-desc), "{conversation.lower()}")] | ' \
+            f'//android.view.View[contains(lower-case(@content-desc), "{conversation.lower()}")]'
+    driver.driver.find_elements(by=AppiumBy.XPATH, value=xpath)[-1].click()
     print(f'Clicking on conversation {conversation} with driver {driver}')
 
-
-def go_to_about(driver: PumaDriver):
-    driver.click(CONVERSATION_STATE_HAMBURGER_MENU)
-    driver.click(CONVERSATION_STATE_ABOUT_BUTTON)
-
-
 class ChatState(SimpleState, ContextualState):
+    """
+    A state representing a chat screen in the application.
+
+    This class extends both SimpleState and ContextualState to represent a chat screen
+    and validate its context based on the conversation name.
+    """
+
     def __init__(self, parent_state):
+        """
+        Initializes the ChatState with a parent state.
+
+        :param parent_state: The parent state of this chat state.
+        """
         super().__init__("Chat screen",
                          xpaths=[CHAT_STATE_CONVERSATION_NAME, CHAT_STATE_MICROPHONE_BUTTON, CHAT_STATE_TEXT_FIELD],
                          parent_state=parent_state)
 
     def validate_context(self, driver: PumaDriver, conversation: str = None) -> bool:
+        """
+        Validates the context of the chat state.
+
+        This method checks if the current chat screen matches the expected conversation name.
+
+        :param driver: The PumaDriver instance used to interact with the application.
+        :param conversation: The name of the conversation to validate against.
+        :return: True if the context is valid, False otherwise.
+        """
         if not conversation:
             return True
 
         content_desc = (driver.get_element(CHAT_STATE_CONVERSATION_NAME).get_attribute('content-desc'))
         return conversation.lower() in content_desc.lower()
 
+@supported_version('4.0.7') # TODO
+class TestFsm(StateGraph): #TODO rename
+    """
+    A class representing a state graph for managing UI states and transitions in the TeleGuard application.
 
-class TestFsm(StateGraph):
-    # TODO: infer name from attribute name (here: state1)
+    This class uses a state machine approach to manage transitions between different states
+    of the TeleGuard user interface. It provides methods to navigate between states, validate states,
+    and handle unexpected states or errors.
+    """
+
+    # TODO: Infer name from attribute name (here: state1)
     conversations_state = SimpleState("Conversation screen", [CONVERSATION_STATE_TELEGUARD_HEADER, CONVERSATION_STATE_HAMBURGER_MENU, CONVERSATION_STATE_TELEGUARD_STATUS], initial_state=True)
     chat_state = ChatState(conversations_state)
     settings_state = SimpleState("Settings", ['//android.view.View[@content-desc="Change TeleGuard ID"]'], parent_state=conversations_state)
     about_screen_state = SimpleState("About", ['//android.view.View[@content-desc="About"]', '//android.view.View[@content-desc=" Terms of use"]'], parent_state=conversations_state)
 
     conversations_state.to(chat_state, go_to_chat)
-    conversations_state.to(settings_state, go_to_settings)
-    conversations_state.to(about_screen_state, go_to_about)
+    conversations_state.to(settings_state, compose_clicks([CONVERSATION_STATE_HAMBURGER_MENU, CONVERSATION_STATE_SETTINGS_BUTTON]))
+    conversations_state.to(about_screen_state, compose_clicks([CONVERSATION_STATE_HAMBURGER_MENU, CONVERSATION_STATE_ABOUT_BUTTON]))
 
     def __init__(self, device_udid):
         """
-        Class with an API for TeleGuard using Appium. Can be used with an emulator or real device attached to the computer.
+        Initializes the TestFsm with a device UDID.
+
+        This class provides an API for interacting with the TeleGuard application using Appium.
+        It can be used with an emulator or a real device attached to the computer.
+
+        :param device_udid: The unique device identifier for the Android device.
         """
         StateGraph.__init__(self, device_udid, APPLICATION_PACKAGE)
         self.add_popup_handler(simple_popup_handler('bah'))
@@ -71,7 +102,10 @@ class TestFsm(StateGraph):
     @action(chat_state)
     def send_message(self, msg: str, conversation: str = None):
         """
-        Sends a message
+        Sends a message in the current chat conversation.
+
+        :param msg: The message to send.
+        :param conversation: The name of the conversation to send the message in.
         """
         print(f"Sending message {msg}")
         self.driver.click(CHAT_STATE_TEXT_FIELD)
@@ -81,8 +115,9 @@ class TestFsm(StateGraph):
     @action(conversations_state)
     def add_contact(self, id: str):
         """
-        Add a contact by TeleGuard ID.
-        :param id: The teleguard ID
+        Adds a contact by TeleGuard ID.
+
+        :param id: The TeleGuard ID of the contact to add.
         """
         self.driver.click(CONVERSATION_STATE_HAMBURGER_MENU)
         self.driver.click('//android.widget.ImageView[@content-desc="Add contact"]')
@@ -92,12 +127,12 @@ class TestFsm(StateGraph):
     @action(conversations_state)
     def accept_invite(self):
         """
-        Accept an invite from another user. If you have multiple invites, only one invite will be accepted (the topmost
-        invite in the UI).
+        Accepts an invite from another user.
+
+        If there are multiple invites, only the topmost invite in the UI will be accepted.
         """
         self.driver.swipe_to_click_element('//android.view.View[contains(@content-desc, "You have been invited")]')
         self.driver.click('//android.widget.Button[@content-desc="ACCEPT INVITE"]')
-
 
 if __name__ == '__main__':
     t = TestFsm('34281JEHN03866')
