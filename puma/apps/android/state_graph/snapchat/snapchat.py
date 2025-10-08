1deaefac45fd6ecdfdef51e49593363817a44f65
isellis <111367432+isellis@users.noreply.github.com>
2025-10-08 10:39:38 +0200

    add progress on snapchat.py

---
+++ puma/apps/android/state_graph/snapchat/snapchat.py
@@ -0,0 +1,8 @@
+from puma.state_graph.app_template import APPLICATION_PACKAGE
+from puma.state_graph.state import SimpleState
+from puma.state_graph.state_graph import StateGraph
+
+APPLICATION_PACKAGE = 'com.snapchat.android'
+
+class Snapchat(StateGraph):
+    # home_state = SimpleState()
