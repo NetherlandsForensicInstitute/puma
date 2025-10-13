40a880bc1e80ed08d30cffe9ea3aa1d771475a85
isellis <111367432+isellis@users.noreply.github.com>
2025-10-13 09:55:21 +0200

    progress on testing

--- puma/apps/android/state_graph/snapchat/snapchat.py
+++ puma/apps/android/state_graph/snapchat/snapchat.py
@@ -3,12 +3,11 @@ from appium.webdriver.common.appiumby import AppiumBy
 from puma.apps.android.state_graph.snapchat import logger
 from puma.state_graph.app_template import APPLICATION_PACKAGE
 from puma.state_graph.puma_driver import PumaDriver
-from puma.state_graph.state import SimpleState, ContextualState
+from puma.state_graph.state import SimpleState, ContextualState, compose_clicks
 from puma.state_graph.state_graph import StateGraph
 
 APPLICATION_PACKAGE = 'com.snapchat.android'
 
-# TODO: check if these xpaths are good enough or that they are to nested and more should be added
 CHAT_STATE_CONVERSATION_NAME = '//android.widget.TextView[@resource-id="com.snapchat.android:id/conversation_title_text_view"]'
 CHAT_STATE_TEXT_FIELD = '//android.widget.EditText[@resource-id="com.snapchat.android:id/chat_input_text_field"]'
 
@@ -25,7 +24,7 @@ def go_to_chat(driver: PumaDriver, conversation: str):
     """
 
     logger.info(f'Clicking on conversation {conversation} with driver {driver}')
-    xpath = f'//androidx.recyclerview.widget.RecyclerView[@resource-id="com.snapchat.android:id/recycler_view"]/android.widget.FrameLayout/android.view.View[@content-desc="Received"]/android.view.View/javaClass[@text="{conversation.lower()}"]'
+    xpath = f'//androidx.recyclerview.widget.RecyclerView[@resource-id="com.snapchat.android:id/recycler_view"]//javaClass[@text="{conversation}"]'
     driver.driver.find_elements(by=AppiumBy.XPATH, value=xpath)[-1].click()
 
 class SnapchatChatState(SimpleState, ContextualState):
@@ -59,13 +58,20 @@ class SnapchatChatState(SimpleState, ContextualState):
             return True
 
         content_desc = (driver.get_element(CHAT_STATE_CONVERSATION_NAME).get_attribute('content-desc'))
-        return conversation.lower() in content_desc.lower()
+        return conversation in content_desc
 
 class Snapchat(StateGraph):
     camera_state = SimpleState(['//android.widget.FrameLayout[@resource-id="com.snapchat.android:id/camera_page"]'], initial_state=True)
     conversation_state = SimpleState(['//android.widget.FrameLayout[@resource-id="com.snapchat.android:id/feed_new_chat"]'])
     chat_state = SnapchatChatState(parent_state=conversation_state)
 
+    camera_state.to(conversation_state, compose_clicks(['//android.view.ViewGroup[@content-desc="Chat"]']))
     conversation_state.to(chat_state, go_to_chat)
 
     # TODO: continue with send snap and add the rest from the template and contributing for adding a new application
+
+    def __init__(self, device_udid):
+        StateGraph.__init__(self, device_udid, APPLICATION_PACKAGE)
+
+if __name__ == "__main__":
+        app = Snapchat(device_udid="34281JEHN03866")
