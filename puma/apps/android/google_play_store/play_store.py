ee3c8cd9f161d7385a4a4f0640edcb29e5c3b8e6
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-10-16 11:00:52 +0200

    105 Ditch most states for app store, only use intents to open app pages

--- puma/apps/android/google_play_store/play_store.py
+++ puma/apps/android/google_play_store/play_store.py
@@ -1,5 +1,4 @@
 import re
-from time import sleep
 
 from puma.state_graph.action import action
 from puma.state_graph.popup_handler import PopUpHandler
@@ -58,10 +57,6 @@ MANAGE_APP_STATE_SYNC = '//android.widget.TextView[@text="Sync apps to devices"]
 MANAGE_APPS_AND_DEVICES = '//android.widget.TextView[@resource-id="com.android.vending:id/0_resource_name_obfuscated" and @text="Manage apps and device"]'
 
 
-def search_app(driver: PumaDriver, app_name: str = None):
-    driver.send_keys(SEARCH_STATE_SEARCH_BOX, app_name)
-    driver.press_enter()
-
 class AppPage(SimpleState, ContextualState):
     def __init__(self, parent_state):
         """
@@ -72,79 +67,45 @@ class AppPage(SimpleState, ContextualState):
         super().__init__(
             xpaths=[HOME_SCREEN_TABS, APP_PAGE_THREE_DOTS],
             parent_state=parent_state)
+        # keep a dict that tracks which app pages were opened last on which device. This is to make the contextual
+        # validation work in most cases.
+        self.last_opened = {}
 
-    def validate_context(self, driver: PumaDriver, app_name: str = None) -> bool:
-        if not app_name:
+    def _is_valid_package_name(self, package_name: str) -> bool:
+        pattern = r'^[a-z_][a-z0-9_]*(\.[a-z_][a-z0-9_]*)*$'
+        return bool(re.fullmatch(pattern, package_name)) and len(package_name) <= 100
+
+    def open_app_page(self, driver: PumaDriver, package_name: str = None):
+        if not self._is_valid_package_name(package_name):
+            raise ValueError(f'Invalid package name: {package_name}')
+        driver.open_url(f'https://play.google.com/store/apps/details?id={package_name}')
+        self.last_opened[driver.udid] = package_name
+
+    def validate_context(self, driver: PumaDriver, package_name: str = None) -> bool:
+        if not package_name:
             return True
-        actual_app_name = driver.get_element('(//android.widget.TextView[@text])[1]').get_attribute('text') # When app is not installed
-        actual_app_name2 = driver.get_element('(//android.view.View[@text])[9]').get_attribute('content-desc') # When app is installed
-        return app_name.lower() in actual_app_name.lower() or app_name.lower() in actual_app_name2.lower()
-
-class SearchResult(SimpleState, ContextualState):
-    def __init__(self, parent_state):
-        """
-        Initializes the App page state with a given parent state.
-
-        :param parent_state: The parent state of this app page state.
-        """
-        super().__init__(
-            xpaths=[SEARCH_RESULT_STATE_BOX, SEARCH_STATE_SEARCH_ICON, VOICE_SEARCH_BUTTON],
-            parent_state=parent_state)
-
-    def validate_context(self, driver: PumaDriver, app_name: str = None) -> bool:
-        if not app_name:
-            return True
-        actual_search_app_name = driver.get_element('//androidx.compose.ui.platform.ComposeView[@resource-id="com.android.vending:id/0_resource_name_obfuscated"]/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View[1]/android.widget.TextView').get_attribute('text')
-        return app_name.lower() in actual_search_app_name.lower()
-
+        # TODO: decide if we want to validate the package name based on the share menu
+        # Doing so would allow us to ditch the map that keeps track of the opened pages
+        return self.last_opened[driver.udid] == package_name
 
 
 @supported_version("")
 class PlayStore(StateGraph):
     apps_tab_state = SimpleState([ACCOUNT_ICON, HOME_SCREEN_TABS, APPS_TAB_SELECTED], initial_state=True)
-    search_tab_state = SimpleState([ACCOUNT_ICON, HOME_SCREEN_TABS, SEARCH_TAB_SELECTED])
-    games_tab_state = SimpleState([ACCOUNT_ICON, HOME_SCREEN_TABS, GAMES_TAB_SELECTED])
-    books_tab_state = SimpleState([ACCOUNT_ICON, HOME_SCREEN_TABS, BOOKS_TAB_SELECTED])
-
-    search_page_state = SimpleState([SEARCH_STATE_SEARCH_BOX, VOICE_SEARCH_BUTTON], parent_state=search_tab_state)
-    search_result_state = SearchResult(parent_state=search_tab_state)
-
     profile_state = SimpleState([], parent_state=apps_tab_state)
     manage_apps_state = SimpleState([MANAGE_APP_STATE, MANAGE_APP_STATE_SYNC], parent_state=apps_tab_state)
-
     # TODO: Contextual state without parent state is not allowed. However, the app page state does not have a defined parent state.
-    app_page_state = AppPage(parent_state=search_result_state)
+    app_page_state = AppPage(parent_state=apps_tab_state)
 
-    apps_tab_state.from_states([search_tab_state, games_tab_state, books_tab_state, search_result_state, app_page_state], compose_clicks([APPS_TAB_BUTTON], name='click_apps_tab'))
-    search_tab_state.from_states([apps_tab_state, games_tab_state, books_tab_state, app_page_state], compose_clicks([SEARCH_TAB_BUTTON], name='click_search_tab'))
-    games_tab_state.from_states([apps_tab_state, search_tab_state, books_tab_state, search_result_state, app_page_state], compose_clicks([GAMES_TAB_BUTTON], name='click_games_tab'))
-    books_tab_state.from_states([apps_tab_state, search_tab_state, games_tab_state, search_result_state, app_page_state], compose_clicks([BOOKS_TAB_BUTTON], name='click_books_tab'))
-
-    search_tab_state.to(search_page_state, compose_clicks([SEARCH_TAB_BUTTON], name='click_search_tab'))
-    search_page_state.to(search_result_state, search_app)
-    search_result_state.to(app_page_state, compose_clicks([SEARCH_RESULT_STATE_FIRST_RESULT], name='click_first_result'))
-
-    profile_state.from_states([search_tab_state, games_tab_state, books_tab_state, app_page_state], compose_clicks([ACCOUNT_ICON], name='click_profile'))
+    apps_tab_state.to(profile_state, compose_clicks([ACCOUNT_ICON], name='click_profile'))
     profile_state.to(manage_apps_state, compose_clicks([MANAGE_APPS_AND_DEVICES], name='click_manage_apps_and_devices'))
-
-    # search_tab_state.to(app_page_state, None)  # TODO: transition: as action or somehow else? State accessible from anywhere by opening the URL?
+    app_page_state.from_states([apps_tab_state, profile_state, manage_apps_state], app_page_state.open_app_page)
 
     def __init__(self, device_udid):
         StateGraph.__init__(self, device_udid, APPLICATION_PACKAGE)
         self.add_popup_handler(PAYMENT_POPUP_HANDLER)
         self.add_popup_handler(LOCAL_RECOMMENDATIONS_POPUP_HANDLER)
         self.add_popup_handler(TRY_GOOGLE_PASS_POPUP_HANDLER)
-
-    # TODO: do we want this kind of action that is not based on UI actions?
-    def _is_valid_package_name(self, package_name: str) -> bool:
-        pattern = r'^[a-z_][a-z0-9_]*(\.[a-z_][a-z0-9_]*)*$'
-        return bool(re.fullmatch(pattern, package_name)) and len(package_name) <= 100
-
-    def open_app_page(self, package_name: str):
-        if not self._is_valid_package_name(package_name):
-            raise ValueError(f'Invalid package name for app: {package_name}')
-        self.driver.open_url(f'https://play.google.com/store/apps/details?id={package_name}')
-        self.current_state = PlayStore.app_page_state
 
     @action(app_page_state)
     def open_app(self, app_name: str = None):
