47839bd663d463b790fe2a50ec621bec7f27a987
carly <c.bakker2@nfi.nl>
2025-11-12 14:50:20 +0100

    123: Process more review comments.

--- puma/apps/android/snapchat/snapchat.py
+++ puma/apps/android/snapchat/snapchat.py
@@ -1,5 +1,6 @@
 from puma.apps.android.snapchat.xpaths import *
 from puma.state_graph.action import action
+from puma.state_graph.popup_handler import PopUpHandler
 from puma.state_graph.puma_driver import PumaDriver, supported_version
 from puma.state_graph.state import SimpleState, ContextualState, compose_clicks
 from puma.state_graph.state_graph import StateGraph
@@ -19,8 +20,22 @@ def go_to_chat(driver: PumaDriver, conversation: str):
     :param conversation: The name of the conversation to navigate to.
     """
 
-    driver.get_elements(CHAT_CONVERSATION.format(conversation=conversation))[-1].click()
-    driver.click(FRIEND_ACTION)
+    driver.click(CHAT_CONVERSATION.format(conversation=conversation))
+
+
+def go_to_snap(driver: PumaDriver, caption: str = None):
+    """
+    Navigates to the snap state.
+
+    This function optionally sets a caption on the photo that was just taken.
+
+    :param driver: The PumaDriver instance used to interact with the application.
+    :param caption: The caption to add on the photo.
+    """
+    if caption:
+        Snapchat.add_caption(driver, caption)
+    driver.click(SENT_TO)
+
 
 class SnapchatChatState(SimpleState, ContextualState):
     """
@@ -54,38 +69,6 @@ class SnapchatChatState(SimpleState, ContextualState):
 
         return driver.is_present(CONVERSATION_TITLE_TEXT.format(conversation=conversation.lower()))
 
-class SnapchatChatSnapState(SimpleState, ContextualState):
-    """
-    A state representing a snap screen, a list of all conversations to send a snap to, in the application.
-
-    This class extends both SimpleState and ContextualState to represent a snap screen
-    and validate its context based on the conversation name.
-    """
-
-    def __init__(self, parent_state):
-        """
-        Initializes the SnapState with a parent state.
-
-        :param parent_state: The parent state of this snap state.
-        """
-        super().__init__(xpaths=[NEW_STORY],
-                         parent_state=parent_state)
-
-    def validate_context(self, driver: PumaDriver, conversation: str = None) -> bool:
-        """
-        Validates the context of the snap state.
-
-        This method checks if the current snap screen matches the expected conversation name.
-
-        :param driver: The PumaDriver instance used to interact with the application.
-        :param conversation: The name of the conversation to validate against.
-        :return: True if the context is valid, False otherwise.
-        """
-        if not conversation:
-            return True
-
-        return driver.is_present(CHAT_CONVERSATION_SEND_TO.format(conversation=conversation.lower()))
-
 
 @supported_version('12.89.0.40')
 class Snapchat(StateGraph):
@@ -99,18 +82,13 @@ class Snapchat(StateGraph):
     camera_state = SimpleState([CAMERA_PAGE], initial_state=True)
     conversation_state = SimpleState([FEED_NEW_CHAT], parent_state=camera_state)
     chat_state = SnapchatChatState(parent_state=conversation_state)
-    captured_state = SimpleState([SENT_TO])
-    caption_state = SimpleState([CAPTION_EDIT], parent_state=captured_state)
-    snap_state = SnapchatChatSnapState(parent_state=captured_state)
-    discard_state = SimpleState([ALERT_DIALOG_DESCRIPTION])
+    captured_state = SimpleState([SENT_TO], parent_state=camera_state)
+    snap_state = SimpleState([NEW_STORY], parent_state=captured_state)
 
     camera_state.to(conversation_state, compose_clicks([CHAT_TAB], name='press_chat_tab'))
     conversation_state.to(chat_state, go_to_chat)
     camera_state.to(captured_state, compose_clicks([CAMERA_CAPTURE], name='press_camera_capture'))
-    captured_state.to(caption_state, compose_clicks([FULL_SCREEN_SURFACE_VIEW], name='press_screen_surface'))
-    captured_state.to(snap_state, compose_clicks([SENT_TO], name='press_sent_to'))
-    captured_state.to(discard_state, compose_clicks([DISCARD_BUTTON], name='press_discard_button'))
-    discard_state.to(camera_state, compose_clicks([DISCARD_ALERT_DIALOG_DISCARD_VIEW], name='press_discard_alert'))
+    captured_state.to(snap_state, go_to_snap)
 
     def __init__(self, device_udid):
         """
@@ -122,6 +100,7 @@ class Snapchat(StateGraph):
         :param device_udid: The unique device identifier for the Android device.
         """
         StateGraph.__init__(self, device_udid, APPLICATION_PACKAGE)
+        self.add_popup_handler(PopUpHandler([ALERT_DIALOG_DESCRIPTION], DISCARD_ALERT_DIALOG_DISCARD_VIEW))
 
     @action(chat_state)
     def send_message(self, message: str, conversation: str = None):
@@ -143,22 +122,15 @@ class Snapchat(StateGraph):
         """
         self.driver.click(TOGGLE_CAMERA)
 
-    @action(camera_state)
-    def take_photo(self, caption: str = None):
-        """
-        Takes a photo.
+    @staticmethod
+    def add_caption(driver: PumaDriver, caption: str):
+        driver.click(FULL_SCREEN_SURFACE_VIEW)
+        caption_field = driver.get_element(CAPTION_EDIT)
+        caption_field.send_keys(caption)
+        driver.click(FULL_SCREEN_SURFACE_VIEW)
 
-        :param caption: The caption to add on the photo.
-        """
-        self.driver.click(CAMERA_CAPTURE)
-        if caption:
-            self.driver.click(FULL_SCREEN_SURFACE_VIEW)
-            caption_field = self.driver.get_element(CAPTION_EDIT)
-            caption_field.send_keys(caption)
-            self.driver.back()
-
-    @action(snap_state)
-    def send_snap_to(self, recipients: list[str]):
+    @action(snap_state, end_state=conversation_state)
+    def send_snap_to(self, recipients: list[str], caption: str = None):
         """
         Sends a snap to recipients.
 
@@ -168,7 +140,7 @@ class Snapchat(StateGraph):
             self.driver.click(RECIPIENTS_TO_ADD.format(recipient=recipient))
         self.driver.click(SEND_BUTTON)
 
-    @action(snap_state)
+    @action(snap_state, end_state=camera_state)
     def send_snap_to_my_story(self):
         """
         Sends a snap to my story.
